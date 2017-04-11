451650cd39bbad4205b0b3bfef08b0a0c78d2588
Sean George <sean_george@byu.edu>
2017-04-11 14:50:13 -0600

    Add direction into calculating bid

--- driver.py
+++ driver.py
@@ -5,7 +5,8 @@ import sys
 import json
 import requests
 import random
-import Order
+import directions_api
+from order import Order
 # here are many imports from flask you may want to use
 from flask import Flask, redirect, request, make_response, send_from_directory, render_template
 
@@ -13,9 +14,19 @@ from flask import Flask, redirect, request, make_response, send_from_directory, 
 app = Flask(__name__)
 
 # The correlation_id of the order will be the key and the Order object the value
-orders = dict()
+orders_received = dict()
+delivered_orders = dict()
 driver_name = random.randint(0,999999)
 port = 5000
+
+def generate_location():
+    latitude = random.uniform(35,40)
+    longitude = random.uniform(-110,-80)
+    location = str(latitude) + "," + str(longitude)
+    return location
+
+location = generate_location()
+print location
 
 # Endpoint to receive an order from a flower shop
 @app.route('/order')
@@ -24,7 +35,8 @@ def send_bid():
 	flower_shop_url = request.args.get("uri")
 	location = request.args.get("location")
 	id = request.args.get("id")
-	order = Order.Order(flower_shop_url, location, id)
+	order = Order(flower_shop_url, location, id)
+	orders_received[id] = order
 	bid = calculate_bid(order)
 	get_params = dict()
 	get_params["drivername"] = driver_name
@@ -49,23 +61,22 @@ def register():
 	return create_response(response)
 
 def calculate_bid(order):
-	return 95
+    bid =  directions_api.secondsA2B(location, order.location)
+    temp = get_weather(order.location)
 
-# Call Google Maps API
-def get_distance(location):
-	return "Unimplemented"
+    if temp < 40:
+        bid += 10 * 40 - temp
+    elif temp > 80:
+        bid += 12 * 80 - temp
+
+    return bid
 
 # Call Weather Underground API
 def get_weather(location):
-	return "Unimplemented"
-
-# Send the
-def deliver_order(order):
-	return "Unimplemented"
+	return 81
 
 def create_response(response):
 	return (response.text, response.status_code, response.headers.items())
-
 
 
 if __name__ == '__main__':
